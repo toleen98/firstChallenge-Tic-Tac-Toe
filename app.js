ea941de45b0660978e7d66029309f9f84160f813
toleen98 <toleenballool98@gmail.com>
2020-06-18 17:40:40 +0300

    creat a function to check if ther is any row matches

--- app.js
+++ app.js
@@ -5,11 +5,19 @@ $('td').one('click',function(){
     $(this).text(turn);
 
     //check if there is any 3 in a row
+    if(isRow()) {
+        //alert with the winner name
+        setTimeout(() => {
+            alert(turn + ' is the winner');
+
+            //then reset the game
+            reset();
+        }, 100);       
+    }
     //check if the table is full
-    if(isFull()) {
+    else if(isFull()) {
         //if full => restart the game
-        reset();
-        
+        reset();    
     }
     else{
         //to toggle to the next player
@@ -31,21 +39,65 @@ function toggle() {
 
 // function to check if table is full
 function isFull() {
-var allTd = $('td');
+    var allTd = $('td');
 
-//iterat over all td
-for (var td of allTd) {
-    if (!$(td).html()) {
-         return false;     
+    //iterat over all td
+    for (var td of allTd) {
+        if (!$(td).html()) {
+            return false;     
+        }   
     }
-    
-}
-return true
+    return true;
 }
 
 // reset the game functhion 
 function reset() {
-   $('td').empty()
-
-    
+   $('td').empty();    
 }
+
+// funchion to check if ther is any row match
+function isRow() {
+    // iterat 3 times to check for the the 3 cols and 3 rows
+    for (var i = 1; i <= 3; i++) {
+        var row = $('#row'+i+' td');
+        var col = $('.col'+i);
+       
+        if (onCheck(row)) {
+            return true;
+        }
+        if(onCheck(col)) {
+            return true
+        }    
+    }
+
+    // check for both left and right cross
+    var rightCross = $('.rightCross');
+    var leftCross = $('.leftCross');
+
+    if (onCheck(rightCross)) {
+        return true;
+    }
+    if(onCheck(leftCross)) {
+        return true
+    }    
+
+    return false;
+
+}
+
+
+// function to check if there is any row matches
+function onCheck(row) {
+    var count = 0
+
+    $(row).each(function (){
+        if($(this).text() === turn) {
+            count++;
+        }
+    });
+
+    if (count === 3) {
+        return true;
+    }
+}
+
